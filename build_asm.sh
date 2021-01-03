5f1414c210eb5b147b3e46961c27e854b3a759a2
jordism <jordism@tid.es>
2021-01-03 18:55:41 +0100

    show properly

--- build_asm.sh
+++ build_asm.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Remember to install 'Pillow==7.1.1', 'argparse'..."
-png2sp1sprite cat_sprites.png -m cat_sprites_mask.png -i sprite_protar -f 24 > ./circle_sprite_masked.asm
+#python png2sp1sprite.py cat_sprites.png -m cat_sprites_mask.png -i sprite_protar -f 24
+python png2sp1sprite.py cat_sprites.png -m cat_sprites_mask.png -i sprite_protar -f 24 > ./circle_sprite_masked.asm
--- circle_masked.c
+++ circle_masked.c
@@ -8,6 +8,7 @@
 #include <z80.h>
 #include <im2.h>
 #include <string.h>
+#include <stdlib.h>
 
 IM2_DEFINE_ISR(isr) {}
 #define TABLE_HIGH_BYTE        ((unsigned int)0xD0)
@@ -21,12 +22,33 @@ extern uint8_t sprite_protar1[];
 extern uint8_t sprite_protar2[];
 extern uint8_t sprite_protar3[];
 
+const uint8_t test[] = {0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55};
+
 struct sp1_Rect full_screen = {0, 0, 32, 24};
+char * chars = "0000000\0";
+
+void print_points(uint16_t points, uint8_t row, uint8_t col) {
+    uint8_t idx;
+    utoa(points, chars, 10);
+    col = 5 - strlen(chars);
+
+    if(col != 0) {
+        for(idx = 0; idx != 5; ++idx) {
+            sp1_PrintAtInv(0, 25 + idx, INK_CYAN | PAPER_BLACK, '0');
+        }
+    }
+    idx = 0;
+    while(chars[idx] != '\0') {
+        sp1_PrintAtInv(0, 25 + idx + col, INK_CYAN | PAPER_BLACK, chars[idx]);
+        ++idx;
+    }
+
+}
 
 int main()
 {
   struct sp1_ss  *circle_sprite;
-  unsigned char x;
+  uint16_t x, y;
 
   memset( TABLE_ADDR, JUMP_POINT_HIGH_BYTE, 257 );
   z80_bpoke( JUMP_POINT,   195 );
@@ -37,36 +59,55 @@ int main()
   zx_border(INK_BLACK);
 
   sp1_Initialize( SP1_IFLAG_MAKE_ROTTBL | SP1_IFLAG_OVERWRITE_TILES | SP1_IFLAG_OVERWRITE_DFILE,
-                  INK_BLACK | PAPER_WHITE,
-                  'X' );
+                  INK_BLACK | PAPER_CYAN,
+                  ' ' );
+  sp1_TileEntry('A', test);
+
   sp1_Invalidate(&full_screen);
 
-  circle_sprite = sp1_CreateSpr(SP1_DRAW_MASK2LB, SP1_TYPE_2BYTE, 4, (int)sprite_protar1, 1);
-  sp1_AddColSpr(circle_sprite, SP1_DRAW_MASK2,    SP1_TYPE_2BYTE, (int)sprite_protar2, 1);
-  sp1_AddColSpr(circle_sprite, SP1_DRAW_MASK2,    SP1_TYPE_2BYTE, (int)sprite_protar3, 1);
+  for (x = 0; x <32; ++x) {
+    for(y = 0; y <24; ++y) {
+        sp1_PrintAt(y, x, INK_BLACK | PAPER_CYAN, 'A');
+    }
+  }
+
+
+  circle_sprite = sp1_CreateSpr(SP1_DRAW_MASK2LB, SP1_TYPE_2BYTE, 4, 0, 1);
+  sp1_AddColSpr(circle_sprite, SP1_DRAW_MASK2,    SP1_TYPE_2BYTE, 64*9, 1);
+  sp1_AddColSpr(circle_sprite, SP1_DRAW_MASK2,    SP1_TYPE_2BYTE, 128*9, 1);
   sp1_AddColSpr(circle_sprite, SP1_DRAW_MASK2RB,  SP1_TYPE_2BYTE, 0, 1);
 
-
   uint16_t  frame = 16;
-  x=0;
+  x=5;
   while(1) {
-    x = 10;
-    sp1_MoveSprAbs(circle_sprite, &full_screen, frame, 4, x, 0, 0);
+    sp1_MoveSprPix(circle_sprite, &full_screen,(int)sprite_protar1+frame - 16, x, 5);
+	sp1_UpdateNow();
     intrinsic_halt();
     intrinsic_halt();
-    intrinsic_halt();
-    sp1_UpdateNow();
+	intrinsic_halt();
 
     if(in_key_pressed(IN_KEY_SCANCODE_0)) {
         frame = 16;
     } else if(in_key_pressed(IN_KEY_SCANCODE_1)) {
         frame += 64;
     } else if(in_key_pressed(IN_KEY_SCANCODE_2)) {
-        frame -= 64;
-    } else if(in_key_pressed(IN_KEY_SCANCODE_4)) {
-        frame += 72;
+        ++x;
+        if(frame == 16) {
+            frame = 80;
+        }  else {
+            frame = 16;
+        }
     }
+
+    if(frame == 528) {
+        frame = 16;
+    }
+
+    if(x > 100) {
+        x = 0;
+    }
+    print_points(frame, 20, 20);
+
     in_wait_nokey();
-    z80_delay_ms(100);
   }
 }
